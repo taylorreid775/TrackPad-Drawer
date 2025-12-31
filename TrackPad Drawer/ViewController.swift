f925c58cec324d25bf66fa71450ae6a2c554eb2d
Taylor Reid <rtt781775@icloud.com>
2025-12-31 11:37:43 -0400

    Initial Commit

---
+++ TrackPad Drawer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TrackPad Drawer
+//
+//  Created by Taylor Reid on 2025-12-31.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ TrackPad DrawerTests/TrackPad_DrawerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackPad_DrawerTests.swift
+//  TrackPad DrawerTests
+//
+//  Created by Taylor Reid on 2025-12-31.
+//
+
+import Testing
+@testable import TrackPad_Drawer
+
+struct TrackPad_DrawerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
